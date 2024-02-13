64f0dafda4972c1759ff6c1e07fb315402f9c157
Idrissa <idiberthe19@gmail.com>
2024-02-12 21:13:56 -0500

    premier push

---
+++ components/navbar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link"
+
+export default function NavBar() {
+
+    return (
+        <div className="flex  space-x-4  text-white ">
+            <button>
+                <Link href="/">
+                    HOME
+                </Link>
+            </button>
+
+            <button>
+                <Link href="/price">
+                    PRICE
+                </Link>
+            </button>
+            <button>
+                <Link href="/dashboard">
+                    DASHBOARD
+                </Link>                </button>
+        </div>
+    )
+}
