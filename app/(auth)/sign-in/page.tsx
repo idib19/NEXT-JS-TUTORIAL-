c2c86311274babe281a6cb9621a8526580cc0715
Idrissa <idiberthe19@gmail.com>
2024-02-20 20:08:41 -0500

    ep6 redirection set-up

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,47 +1,43 @@
 "use client"
 
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useEffect } from 'react';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { app } from '@/firebase.config';
+import { useRouter } from 'next/navigation';
 
-import { app } from "@/firebase.config";
-import { redirect } from "next/navigation";
-import { useRouter } from "next/router";
-
-const email = "idiberthe19@gmail.com"
-const password = "12345678"
-
-export default async function SignIn() {
-
+export default function SignIn() {
     const router = useRouter();
-    const auth = getAuth(app);
 
-    await signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in  
-            const user = userCredential.user;
-            // ...
-            if (user) {
-                console.log("connexion reussi ", user.email);
-                window.location.assign('/')
+    const email = "idiberthe19@gmail.com"
+    const password = "12345678"
+
+    useEffect(() => {
+        const signIn = async () => {
+            const auth = getAuth(app);
+            try {
+                const userCredential = await signInWithEmailAndPassword(auth, email, password);
+                // Signed in
+                const user = userCredential.user;
+                if (user) {
+                    // Redirect to dashboard if there is a user
+                    router.push('/dashboard');
+                }
+            } catch (error) {
+                const errorCode = error;
+                const errorMessage = error;
+                // Handle the error accordingly
+                console.error(errorCode, errorMessage);
+                // Redirect to the home page or show an error message
+                router.push('/'); // Optional: Navigate to '/' or show an error message instead
             }
-            else {
-                console.log("conexion echiuee")
-                redirect('/')
-            }
+        };
 
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // ..
-        });
-
+        signIn();
+    }, [router]); // Add router to the dependency array to avoid re-run
 
     return (
         <div>
-            <h1>
-                PAGE DE CONNEXION
-            </h1>
+            <h1>PAGE DE CONNEXION</h1>
         </div>
-    )
-
+    );
 }
